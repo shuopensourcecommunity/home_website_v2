e345bdf28eb26408c556818a5f8cb96d6524c206
simon <simon@gitcafe.com>
2015-02-09 21:18:45 +0800

    arrange url rules

--- backend/index.py
+++ backend/index.py
@@ -3,6 +3,22 @@ app = Flask(__name__)
 app.debug = True
 
 
+'''
+url rules:
+	/    : home
+	/home: home
+
+	/general :  a bref introduce to our community
+
+	/activity : student activities
+
+	/project:  community project
+
+	/contact:  contanct us
+
+'''
+
+
 @app.route('/')
 def hello_world():
 	return "Hello World!"
