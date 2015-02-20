3422b55bbff8500df6fe6d78260399b496ae871a
simon <bucketzxm@gmail.com>
2015-02-20 14:31:56 +0800

    use fabric to deploy flask

--- backend/index.py
+++ backend/index.py
@@ -1,29 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+
 app.debug = True
 
 
-'''
-url rules:
-	/    : home
-	/home: home
-
-	/general :  a bref introduce to our community
-		/general/member:  introduce to our members
-
-	/activity : student activities
-
-	/project:  community project
-
-	/contact:  contanct us
-
-	/parterner :  our supporter and parterners
-
-'''
-
-
 @app.route('/')
-def hello_world():
+def index():
 	return "Hello World!"
 
 if __name__ == '__main__':
