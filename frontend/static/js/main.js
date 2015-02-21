4ce71682174f92cfb2a50fdd7d591f171acbdd79
NightEndSorcerer <sorcerer.ma@gmail.com>
2015-02-21 16:13:57 +0800

    Fade in about us after show coming soon

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -5,13 +5,26 @@
 $(function() {
     console.log('(*´∀`)~♥ 上海大学开源社区');
 
+    var showAboutUs = function() {
+    	var offset = -472 / 2;
+
+    	$('.wrapper').delay(800).animate({
+    		height: 472,
+    		marginTop: offset
+    	}, 500, function () {
+    		$('#about-us').fadeIn(500);
+    	});
+    };
+
     var theater = new TheaterJS();
 
     theater.describe('comingSoon', {
     	speed: 0.6,
-    	accuracy: 0.6,
+    	accuracy: 0.7,
     	invincibility: 2
     }, '#coming-soon');
 
-    theater.write('comingSoon:New Website Coming Soon...');
+    theater
+    	.write('comingSoon:NEW WEBSITE COMING SOON...')
+    	.write(showAboutUs);
 });
