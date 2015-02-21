575e263f168e8e4ab24b193f1b2af24b6e52a0fb
NightEndSorcerer <sorcerer.ma@gmail.com>
2015-02-21 15:43:51 +0800

    首页改为 coming soon

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -4,4 +4,14 @@
 
 $(function() {
     console.log('(*´∀`)~♥ 上海大学开源社区');
+
+    var theater = new TheaterJS();
+
+    theater.describe('comingSoon', {
+    	speed: 0.6,
+    	accuracy: 0.6,
+    	invincibility: 2
+    }, '#coming-soon');
+
+    theater.write('comingSoon:New Website Coming Soon...');
 });
