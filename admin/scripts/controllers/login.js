0e720787b1242460f66cee3a891c9ea2e1b9700c
simon <bucketzxm@gmail.com>
2015-03-02 17:23:17 +0800

    init admin

--- admin/scripts/controllers/login.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('oscApp')
-	.controller('LoginCtrl', function($scope){
-		// TODO
-});
--- admin/scripts/controllers/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('oscApp')
-	.controller('MainCtrl', function($scope){
-		// TODO
-});
