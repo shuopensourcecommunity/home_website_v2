2829bbf301874f7a34b7fc0080a5aeb4835d84ff
simon <bucketzxm@gmail.com>
2015-03-02 17:41:23 +0800

    change dir struct

---
+++ admin/scripts/controllers/login.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('oscApp')
+.controller('LoginCtrl', function($scope){
+	// TODO
+});
---
+++ admin/scripts/controllers/main.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('oscApp')
+.controller('MainCtrl', function($scope){
+	// TODO
+});
