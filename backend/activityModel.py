be677b40d3e430d44359d71c20da7a04e6894c1f
simon <bucketzxm@gmail.com>
2015-03-09 14:35:59 +0800

    use flask view.modelview to realize restful api

---
+++ backend/activityModel.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+from flask.views import MethodView
+
+class ActivityAPI(MethodView):
+	def get(self):
+		pass
+	def post(self):
+		pass
+	def delete(self):
+		pass
+	def put(self):
+		pass
--- backend/index.py
+++ backend/index.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask.views import MethodView
+
 app = Flask(__name__)
 
 app.debug = True
@@ -8,6 +10,21 @@ app.debug = True
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+
+@app.route('/acitivies/title',methods=['GET','POST'])
+def get_activity_title():
+	if request.method == 'GET':
+		# get title as a dictionary
+		title = {}
+		body = flask.jsonify(**title)
+
+		return flask.jsonify(**title)
+
+
+@app.errorhandler(404)
+def not_found(error):
+	pass
 
 
 if __name__ == '__main__':
