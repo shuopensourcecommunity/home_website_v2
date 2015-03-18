789e56b8c8bdc13484db3dfffe9eefbbeaa80c1b
simon <bucketzxm@gmail.com>
2015-03-18 14:40:03 +0800

    complete activity,project,member,partner get api

--- backend/Api.py
+++ backend/Api.py
@@ -1,11 +1,9 @@
-import model
-
 from flask.views import MethodView
 from flask import jsonify
 from flask import make_response
+import model
 class ActivityAPI(MethodView):
 	def get(self,*args,**kwargs):
-		print self,args,kwargs
 		data = model.Activity.query.filter_by(id=kwargs['user_id']).first()
 		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource'])}
 		data_json = jsonify(data_json)
@@ -20,7 +18,11 @@ class ActivityAPI(MethodView):
 
 class ProjectAPI(MethodView):
 	def get(self,*args,**kwargs):
-		pass
+		data = model.Project.query.filter_by(id=kwargs['project_id']).first()
+		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource'])}
+		data_json = jsonify(data_json)	
+		resp = make_response( data_json,200)
+		return resp
 	def post(self):
 		pass
 	def delete(self):
@@ -31,7 +33,11 @@ class ProjectAPI(MethodView):
 
 class MemberAPI(MethodView):
 	def get(self,*args,**kwargs):
-		pass
+		data = model.Member.query.filter_by(id=kwargs['member_id']).first()
+		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource'])}
+		data_json = jsonify(data_json)
+		resp = make_response( data_json,200)
+		return resp
 	def post(self):
 		pass
 	def delete(self):
@@ -42,7 +48,11 @@ class MemberAPI(MethodView):
 
 class PartnerAPI(MethodView):
 	def get(self,*args,**kwargs):
-		pass
+		data = model.Partner.query.filter_by(id=kwargs['partner_id']).first()
+		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource'])}
+		data_json = jsonify(data_json)
+		resp = make_response(data_json,200)
+		return resp
 	def post(self):
 		pass
 	def delete(self):
