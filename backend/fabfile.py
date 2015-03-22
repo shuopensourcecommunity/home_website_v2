7587b0345c03ed8c51a65bd62e6afe00ad260eae
simon <bucketzxm@gmail.com>
2015-03-22 21:09:57 +0800

    modify fabfile.py

--- backend/fabfile.py
+++ backend/fabfile.py
@@ -1,8 +1,11 @@
 from fabric.api import *
-
+from fabtools import require
 env.user = 'ubuntu'
+env.port = 3868
 
 env.hosts = ['api.shuosc.org']
+
+
 
 def pack():
 	local('python setup.py sdist --formats=gztar',capture=False)
@@ -12,11 +15,12 @@ def deploy():
 	dist = local('python setup.py --fullname',capture=True).strip()
 
 	put('dist/%s.tar.gz'%dist,'/tmp/backend.tar.gz')
-
 	run('mkdir /tmp/backend')
 
-	with cd('/tmp/backend'):
-		run('tar xvf /tmp/backend.tar.gz')
-		run('/var/www/shuosc/backend/venv/bin/python setup.py install')
+	run('tar xvf /tmp/backend.tar.gz -C /tmp/backend')
+	run('cd /tmp/backend/'+dist)
+	run('ls -al')
+	sudo('/var/www/shuosc/backend/venv/bin/python /tmp/backend/'+dist+'/setup.py install --prefix=/var/www/shuosc/backend', user='www-data')
 	run('rm -rf /tmp/backend /tmp/backend.tar.gz')
 	run('touch /var/www/shuosc/backend/shuosc_uwsgi.sock')	
+
