883a9791377d7e801112ab13f7bdebac53d412ab
simon <bucketzxm@gmail.com>
2015-06-05 23:21:53 +0800

    something I forgotten

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -6,17 +6,19 @@ import model
 class ActivityAPI(MethodView):
 	def get(self,*args,**kwargs):
 		data = model.Activity.query.filter_by(id=kwargs['user_id']).first()
-		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource'])}
+		data_json = { kwargs['resource'] : data.__getattribute__(kwargs['resource']),
+                      "code":1,
+                      "msg":"success"}
 		data_json = jsonify(data_json)
 		resp = make_response( data_json,200 )
 		return resp
 	def post(self,*args,**kwargs):
 		pass
-
 	def delete(self):
 		pass
 	def put(self):
 		pass
+
 
 
 class ProjectAPI(MethodView):
